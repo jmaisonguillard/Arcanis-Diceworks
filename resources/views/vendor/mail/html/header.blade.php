d3019dd3785d6b14aafe4d4d15970dfe2d8b3fbf
Rian Morningstar <akos.tattoo.co@gmail.com>
2024-09-14 01:08:28 -0600

    updates to email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+    <img clas src="{{ url('images/emblems/arcanis_diceworks_black.png') }}" class="w-10 h-auto" alt="Arcanis Diceworks">
+</a>
+</td>
+</tr>
