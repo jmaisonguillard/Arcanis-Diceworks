6e696c146f5b90504eb90be2454dfe952cc6953b
Rian Morningstar <akos.tattoo.co@gmail.com>
2024-09-28 14:28:58 -0600

    More updates

--- app/Livewire/CustomizeDice.php
+++ app/Livewire/CustomizeDice.php
@@ -7,64 +7,83 @@ use Livewire\Component;
 class CustomizeDice extends Component
 {
     public $colorsStore, $colorSelection, $flakeSelection, $numberSelection;
-    public $selectedStyle;
+    public $selectedStyle, $selectedColors = [];
     public $openGroups = []; // Track which groups are open
+
+    public $cost = 60;
 
     private $diceStyles = [
         [
             "name" => "Dirty-Pour",
             "description" => "Multiple colors of resin are mixed together in a single pour without fully blending, creating swirls, marbling, or cloudy effects.",
-            "slug" => "dirty-pour"
+            "slug" => "dirty-pour",
+            "max_colors" => 4
+        ],
+        [
+            "name" => "Petri",
+            "description" => "This style involves dropping alcohol inks or pigments into the resin as it cures, creating organic, flowing, and sometimes 'bacterial' or 'fractal' shapes within the dice.",
+            "slug" => "petri",
+            "max_colors" => 3
         ],
         [
             "name" => "Layered",
             "description" => "Multiple layers of resin, each a different color or texture, are poured one at a time to create clear, defined layers within the dice.",
-            "slug" => "layered-poured"
+            "slug" => "layered-poured",
+            "max_colors" => 4
         ],
         [
             "name" => "Ink-Wash",
             "description" => "Pigment is applied to the surface of the dice, then wiped away, leaving a wash effect that enhances the edges and carved numbers.",
-            "slug" => "ink-wash"
+            "slug" => "ink-wash",
+            "max_colors" => 2
         ],
         [
             "name" => "Inclusions",
             "description" => "Small objects, glitter, foil, flowers, or other items are placed inside the resin mold to give depth and uniqueness to each die.",
-            "slug" => "inclusions"
+            "slug" => "inclusions",
+            "max_colors" => 1
         ],
         [
             "name" => "Ombre",
             "description" => "The resin is colored in such a way that it fades from one color to another, either horizontally or vertically within the dice.",
-            "slug" => "ombre-fade"
+            "slug" => "ombre-fade",
+            "max_colors" => 2
         ],
         [
             "name" => "Transparent",
             "description" => "Clear or slightly tinted resin that allows visibility through the entire die, often paired with inclusions or other visual effects.",
-            "slug" => "clear-transparent"
+            "slug" => "clear-transparent",
+            "max_colors" => 2
         ],
         [
             "name" => "Pearlescent",
             "description" => "Mica powder or pearlescent pigments are added to the resin to give a shimmering or metallic look to the dice, often resulting in a marbled or swirling effect.",
-            "slug" => "pearlescent-mica-powder"
+            "slug" => "pearlescent-mica-powder",
+            "max_colors" => 2
         ],
         [
             "name" => "Galaxy",
             "description" => "A blend of dark, often black or deep purple resin, mixed with glitter or stars, creating a space or 'galaxy' effect within the dice.",
-            "slug" => "galaxy-dice"
+            "slug" => "galaxy-dice",
+            "max_colors" => 2
         ],
         [
             "name" => "Chameleon",
             "description" => "These dice use special pigments that change color depending on the light or viewing angle, creating a dynamic and ever-changing appearance.",
-            "slug" => "chameleon-color-shifting"
+            "slug" => "chameleon-color-shifting",
+            "max_colors" => 1
         ],
         [
             "name" => "Glow",
             "description" => "Incorporates pigments or materials that glow in the dark, giving the dice a magical appearance when the lights are out.",
-            "slug" => "glow-in-the-dark"
+            "slug" => "glow-in-the-dark",
+            "max_colors" => 1
         ],
         [
             "name" => "Frosted",
             "description" => "These dice have a frosted or matte finish, which gives them a soft, velvety texture instead of the typical glossy appearance.",
-            "slug" => "frosted-matte-finish"
+            "slug" => "frosted-matte-finish",
+            "max_colors" => 4
         ]
     ];
 
@@ -82,6 +101,10 @@ class CustomizeDice extends Component
             $this->colorsStore = [];
         }
     }
+    public function updatedSelectedStyle()
+    {
+        $this->selectedColors = [];
+    }
 
     // Toggle the visibility of a group
     public function toggleGroup($groupName)
@@ -95,12 +118,49 @@ class CustomizeDice extends Component
         }
     }
 
+    public function toggleColors($type = '', $hex) {
+        $data = [$type, $hex];
+
+        if(!$this->selectedStyle) return;
+        $current_colors_count = count($this->selectedColors);
+
+        if(in_array($data, $this->selectedColors)) {
+            $key = array_search($data, $this->selectedColors);
+
+            if($key !== false) {
+                unset($this->selectedColors[$key]);
+            }
+        }
+
+        $find_style_by_slug = array_filter($this->diceStyles, function ($style) {
+            return $style['slug'] === $this->selectedStyle;
+        });
+
+        $style = array_values($find_style_by_slug);
+        $max_colors = $style[0]['max_colors'];
+
+        if($max_colors === 1) {
+            $this->selectedColors = [];
+            $this->selectedColors[] = $data;
+        }
+
+        if($current_colors_count >= $style[0]['max_colors']) return;
+
+        $this->selectedColors[] = $data;
+    }
+
+    public function isColorSelected($type = '', $hex) {
+        $data = [$type, $hex];
+        return in_array($data, $this->selectedColors);
+    }
+
     public function render()
     {
         return view('livewire.customize-dice', [
             'colors' => $this->colorsStore,
             'diceStyles' => $this->diceStyles,
             'selectedStyle' => $this->selectedStyle,
+            'cost' => $this->cost
         ]);
     }
 }
--- resources/views/livewire/customize-dice.blade.php
+++ resources/views/livewire/customize-dice.blade.php
@@ -2,76 +2,88 @@
     <div class="mx-auto max-w-7xl my-32">
         <div class="flex flex-row gap-x-8">
             <div class="w-1/4">
-{{--                <div class="dice rounded-lg border border-gray-100"--}}
-{{--                     style="background: url({{ asset('images/arcanis-repeat-logo-pw.jpg') }}); background-size: 200%;">--}}
-{{--                    <div class="content">--}}
-{{--                        <div class="die">--}}
-{{--                            <figure class="face face-1"></figure>--}}
-{{--                            <figure class="face face-2"></figure>--}}
-{{--                            <figure class="face face-3"></figure>--}}
-{{--                            <figure class="face face-4"></figure>--}}
-{{--                            <figure class="face face-5"></figure>--}}
-{{--                            <figure class="face face-6"></figure>--}}
-{{--                            <figure class="face face-7"></figure>--}}
-{{--                            <figure class="face face-8"></figure>--}}
-{{--                            <figure class="face face-9"></figure>--}}
-{{--                            <figure class="face face-10"></figure>--}}
-{{--                            <figure class="face face-11"></figure>--}}
-{{--                            <figure class="face face-12"></figure>--}}
-{{--                            <figure class="face face-13"></figure>--}}
-{{--                            <figure class="face face-14"></figure>--}}
-{{--                            <figure class="face face-15"></figure>--}}
-{{--                            <figure class="face face-16"></figure>--}}
-{{--                            <figure class="face face-17"></figure>--}}
-{{--                            <figure class="face face-18"></figure>--}}
-{{--                            <figure class="face face-19"></figure>--}}
-{{--                            <figure class="face face-20"></figure>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                {{--                <div class="dice rounded-lg border border-gray-100"--}}
+                {{--                     style="background: url({{ asset('images/arcanis-repeat-logo-pw.jpg') }}); background-size: 200%;">--}}
+                {{--                    <div class="content">--}}
+                {{--                        <div class="die">--}}
+                {{--                            <figure class="face face-1"></figure>--}}
+                {{--                            <figure class="face face-2"></figure>--}}
+                {{--                            <figure class="face face-3"></figure>--}}
+                {{--                            <figure class="face face-4"></figure>--}}
+                {{--                            <figure class="face face-5"></figure>--}}
+                {{--                            <figure class="face face-6"></figure>--}}
+                {{--                            <figure class="face face-7"></figure>--}}
+                {{--                            <figure class="face face-8"></figure>--}}
+                {{--                            <figure class="face face-9"></figure>--}}
+                {{--                            <figure class="face face-10"></figure>--}}
+                {{--                            <figure class="face face-11"></figure>--}}
+                {{--                            <figure class="face face-12"></figure>--}}
+                {{--                            <figure class="face face-13"></figure>--}}
+                {{--                            <figure class="face face-14"></figure>--}}
+                {{--                            <figure class="face face-15"></figure>--}}
+                {{--                            <figure class="face face-16"></figure>--}}
+                {{--                            <figure class="face face-17"></figure>--}}
+                {{--                            <figure class="face face-18"></figure>--}}
+                {{--                            <figure class="face face-19"></figure>--}}
+                {{--                            <figure class="face face-20"></figure>--}}
+                {{--                        </div>--}}
+                {{--                    </div>--}}
+                {{--                </div>--}}
             </div>
             <div class="w-3/4">
                 <div class="flex flex-row justify-between">
                     <h1 class="text-xl font-medium text-gray-900">{{ __('Customize your Dice') }}</h1>
-                    <h1 class="text-xl font-medium text-gray-900">$60</h1>
+                    <h1 class="text-xl font-medium text-gray-900">${{ number_format($cost, 2) }}</h1>
                 </div>
                 <div>
-                    <form>
-                        <div class="text-sm font-medium text-gray-900">{{ __('Style') }}</div>
-                        <div class="mt-2 grid grid-cols-3 gap-3 sm:grid-cols-3">
-                            @foreach($diceStyles as $style)
-                                <label aria-label="{{ $style['name'] }}" aria-description="{{ $style['description'] }}"
-                                       class="relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none">
-                                    <input type="radio" wire:model.live="selectedStyle" name="dice-style" value="{{ $style['slug'] }}" class="sr-only">
-                                    <span class="flex flex-1">
+                    <div class="text-sm font-medium text-gray-900 mt-8">{{ __('Style') }}</div>
+                    <div class="mt-2 grid grid-cols-3 gap-3 sm:grid-cols-3">
+                        @foreach($diceStyles as $style)
+                            <label aria-label="{{ $style['name'] }}" aria-description="{{ $style['description'] }}"
+                                   class="relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none">
+                                <input type="radio" wire:model.live="selectedStyle" name="dice-style"
+                                       value="{{ $style['slug'] }}" class="sr-only">
+                                <span class="flex flex-1">
                                         <span class="flex flex-col">
-                                            <span class="block text-sm font-medium text-gray-900">{{ $style['name'] }}</span>
-                                            <span class="mt-1 flex items-center text-sm text-gray-500">{{ $style['description'] }}</span>
+                                            <span
+                                                class="block text-sm font-medium text-gray-900">{{ $style['name'] }}
+                                            </span>
+                                            <span
+                                                class="mt-1 flex items-center text-sm text-gray-500">{{ $style['description'] }}
+                                            </span>
+                                            <span class="text-xs font-semibold text-gray-500">Max Colors - {{ $style['max_colors'] }}</span>
                                         </span>
                                     </span>
-                                    <svg class="h-5 w-5 text-indigo-600 fill-heliotrope {{ $selectedStyle !== $style['slug'] ? 'invisible' : 'visible' }}" viewBox="0 0 20 20" fill="currentColor"
-                                         aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
-                                    <!--
-                                      Active: "border", Not Active: "border-2"
-                                      Checked: "border-indigo-600", Not Checked: "border-transparent"
-                                    -->
-                                    <span class="pointer-events-none absolute -inset-px rounded-lg border-2 {{ $selectedStyle === $style['slug'] ? 'border border-heliotrope' : 'border-2 border-transparent' }}"
-                                          aria-hidden="true"></span>
-                                </label>
-                            @endforeach
-                        </div>
-                    </form>
+                                <svg
+                                    class="h-5 w-5 text-indigo-600 fill-heliotrope {{ $selectedStyle !== $style['slug'] ? 'invisible' : 'visible' }}"
+                                    viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                                <!--
+                                  Active: "border", Not Active: "border-2"
+                                  Checked: "border-indigo-600", Not Checked: "border-transparent"
+                                -->
+                                <span
+                                    class="pointer-events-none absolute -inset-px rounded-lg border-2 {{ $selectedStyle === $style['slug'] ? 'border border-heliotrope' : 'border-2 border-transparent' }}"
+                                    aria-hidden="true"></span>
+                            </label>
+                        @endforeach
+                    </div>
                 </div>
-                <div class="divide-y divide-gray-200 gap-y-8">
+                <div class="divide-y divide-gray-200 gap-y-8 mt-8">
                     <div>
                         <div class="text-sm font-medium text-gray-900">{{ __('Select Colors') }}</div>
-                        @foreach($colors->dice->powder->mica as $color)
-
-                        @endforeach
+                        <div>
+                            <div class="font-bold mt-4">Pearlescent Micas</div>
+                        </div>
+                        <div class="mt-6 flex items-center gap-3 flex flex-row flex-wrap">
+                            @foreach($colors->dice->powder->mica as $color)
+                                <div class="w-10 h-10 rounded-full shadow-md border border-gray-200 cursor-pointer user-select-none {{ $this->isColorSelected('powder.mica', $color->hex) ? 'ring ring-offset-1' : '' }}" wire:click="toggleColors('powder.mica', '{{ $color->hex }}')" style="background-color: {{ $color->hex }}; {{ $this->isColorSelected('powder.mica', $color->hex) ? '--tw-ring-color: ' . $color->hex : '' }}" title="{{ $color->name }}"></div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
